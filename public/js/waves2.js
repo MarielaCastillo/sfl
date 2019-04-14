7ecdc3a6653d08815c568a07a69f05563ea4a5a1
M C <mariela.castillo.mc2@gmail.com>
2019-04-13 18:09:14 -0700

    Parallax Home
    Fixes. Sun dissapears and reappears.
    Less scrolling

--- public/js/waves2.js
+++ public/js/waves2.js
@@ -2,7 +2,11 @@
 
 $(window).scroll(function(){
     var scrollVar = $(window).scrollTop();
+    if (scrollVar < 700) {
     $('.fadein').css("opacity", 0.2 + scrollVar/300);
+    }
+    else
+    $('.fadein').css("opacity", 0);
 })
 
 $(window).scroll(function(){
@@ -12,11 +16,25 @@ $(window).scroll(function(){
 
 $(window).scroll(function(){
     var scrollVar = $(window).scrollTop();
+    if (scrollVar < 400) {
     $('.fadein3').css("opacity", 0.5 + scrollVar/300);
+    }
 })
 
 $(window).scroll(function(){
     var scrollVar = $(window).scrollTop();
     $('.fadeout').css("opacity", 2.1 - scrollVar/500);
 })
+
+$(window).scroll(function(){
+    var scrollVar = $(window).scrollTop();
+    if (scrollVar > 700) {
+    $('.dissappear').css("display", "none");
+    }
+    else
+    {
+    $('.dissappear').css("display", "block");
+    }
+})
+
 
